681b5146edaf7335e522304483fd591152fe3030
luis henry <lhreyes5@misena.edu.co>
2018-08-31 08:45:50 -0500

    henry 2

--- app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decoracionNavegadorBar = getWindow().getDecorView();
+
+        int opciones = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decoracionNavegadorBar.setSystemUiVisibility(opciones);
 
         new Handler().postDelayed(new Runnable() {
             @Override
