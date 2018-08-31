17de3bac624defab7e495c42db5e0914b1290dd5
Brahiam Musse <bamusse@misena.edu.co>
2018-08-31 14:13:28 -0500

    5

--- app/src/main/java/com/example/worldskills/turisapp/PrincipalActivity.java
+++ app/src/main/java/com/example/worldskills/turisapp/PrincipalActivity.java
@@ -1,13 +1,11 @@
 package com.example.worldskills.turisapp;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentTransitionImpl;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
